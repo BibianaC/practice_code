4997ef08c088c291478fb6e7a65ddeac620f0296
bibiana <bcristofol@gmail.com>
2015-01-29 13:19:11 +0000

    Added loser, winning message method

--- js_RPS/js/Game.js
+++ js_RPS/js/Game.js
@@ -24,3 +24,25 @@ Game.prototype.winner = function(){
     return this.player2;
   }
 }
+
+Game.prototype.loser = function() {
+  return (this.winner() === this.player1 ? this.player2 : this.player1);
+}
+
+Game.prototype.winningMessage = function() {
+  var message;
+
+  if(this.winner()) {
+    message = [this.winner().name,
+    this._victoryVerbFor(this.winner().pick, this.loser().pick),
+    this.loser().name].join(' ');
+  } else {
+    message = 'Draw';
+  }
+
+  return message;
+}
+
+Game.prototype._victoryVerbFor = function(pick, opponentPick) {
+  return this.PAIRS[pick][opponentPick];
+}
