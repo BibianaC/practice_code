507b526736c73412f1443b9fd535ec671211d67d
bibiana <bcristofol@gmail.com>
2015-04-15 09:54:52 +0100

    Refactor - Extract classes to a lib folder

--- the_cucumber_book/atm/features/step_definitions/steps.rb
+++ the_cucumber_book/atm/features/step_definitions/steps.rb
@@ -15,45 +15,13 @@ end
 
 World(KnowsMyAccount)
 
-class Account
-  def deposit(amount) 
-    @balance = amount
-  end
-
-  def balance
-    @balance
-  end
-
-end
-
-class Teller
-  def initialize(cash_slot)
-    @cash_slot = cash_slot
-  end
-
-  def withdraw_from(account, amount)
-    @cash_slot.dispense(amount)
-  end
-end
-
-class CashSlot
-  def contents
-    @contents or raise("I'm empty!")
-  end
-
-  def dispense(amount)
-    @contents = amount
-  end
-  
-end
-
 CAPTURE_CASH_AMOUNT = Transform /^\$(\d+)$/ do |digits| 
   digits.to_i
 end
 
 Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount| 
   my_account.deposit(amount)
-  my_account.balance.should eq(amount),
+  expect(my_account.balance).to eq(amount),
   "Expected the balance to be #{amount} but it was #{my_account.balance}" 
 end
 
